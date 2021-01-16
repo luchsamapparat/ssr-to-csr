6f6deae49a8867a24c3167ba757711f93e4dbf05
Marvin Luchs <marvin@luchs.org>
2021-01-17 00:01:23 +0100

    fix: submit form as fallback when fetch fails

--- src/main/resources/static/utils.js
+++ src/main/resources/static/utils.js
@@ -5,14 +5,14 @@
  * @returns {Promise<HTMLDocument>}
  */
 export async function submitForm(form) {
-    const response = await fetch(form.action, {
-        method: form.method,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        // @ts-ignore https://github.com/microsoft/TypeScript/issues/30584
-        body: new URLSearchParams(new FormData(form).entries()).toString()
-    });
+    let response;
+    try {
+        response = await submitUsingFetch(form);
+    } catch (error) {
+        form.submit();
+        return;
+    }
+
     const responseDocument = new DOMParser().parseFromString(await response.text(), 'text/html');
 
     if (!response.ok) {
@@ -20,6 +20,17 @@ export async function submitForm(form) {
     }
 
     return responseDocument;
+}
+
+async function submitUsingFetch(form) {
+    return fetch(form.action, {
+        method: form.method,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        // @ts-ignore https://github.com/microsoft/TypeScript/issues/30584
+        body: new URLSearchParams(new FormData(form).entries()).toString()
+    });
 }
 
 /**
