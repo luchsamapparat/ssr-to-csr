a7eb362dcaa766511c7e993b6d5a1eefb26489df
Marvin Luchs <marvin.luchs@holisticon.de>
2021-01-24 22:17:10 +0100

    refactor: move JSON API into own controller

---
+++ src/main/java/org/luchs/marvin/ssrtodo/TodoApiController.java
@@ -0,0 +1,47 @@
+package org.luchs.marvin.ssrtodo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@Controller
+public class TodoApiController {
+
+    @Autowired
+    private TodoListService todoListService;
+
+    @GetMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getTasksJson() {
+        return ResponseEntity.ok(todoListService.getTasks());
+    }
+
+    @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity addTaskJson(@Valid @RequestBody TaskForm taskForm) {
+        todoListService.addTask(taskForm.getDescription(), taskForm.getDueDate());
+        return ResponseEntity
+            .created(URI.create("/tasks"))
+            .build();
+    }
+
+    @PostMapping(value = "/tasks/completed", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity completeTasksJson(@RequestBody CompletedTasksForm completedTasksForm) {
+        todoListService.completeTasks(completedTasksForm.getCompletedTasks());
+        return ResponseEntity
+            .status(HttpStatus.SEE_OTHER)
+            .location(URI.create("/tasks"))
+            .build();
+    }
+
+}
--- src/main/java/org/luchs/marvin/ssrtodo/TodoController.java
+++ src/main/java/org/luchs/marvin/ssrtodo/TodoController.java
@@ -3,17 +3,14 @@ package org.luchs.marvin.ssrtodo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -32,11 +29,6 @@ public class TodoController {
         return redirectToTasks();
     }
 
-    @GetMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getTasksJson() {
-        return ResponseEntity.ok(todoList.getTasks());
-    }
-
     @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ModelAndView processTaskForm(@Valid TaskForm taskForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -49,14 +41,6 @@ public class TodoController {
         return redirectToTasks();
     }
 
-    @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity addTaskJson(@Valid @RequestBody TaskForm taskForm) {
-        addTask(taskForm);
-        return ResponseEntity
-            .created(URI.create("/tasks"))
-            .build();
-    }
-
     @GetMapping("/tasks/completed")
     public ModelAndView getCompletedTasks() {
         return redirectToTasks();
@@ -66,15 +50,6 @@ public class TodoController {
     public ModelAndView processCompleteTasksForm(@RequestParam("completedTasks[]") List<String> taskIds) {
         todoListService.completeTasks(taskIds);
         return redirectToTasks();
-    }
-
-    @PostMapping(value = "/tasks/completed", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity completeTasksJson(@RequestBody CompletedTasksForm completedTasksForm) {
-        completeTasks(completedTasksForm.getCompletedTasks());
-        return ResponseEntity
-            .status(HttpStatus.SEE_OTHER)
-            .location(URI.create("/tasks"))
-            .build();
     }
 
     private ModelAndView redirectToTasks() {
