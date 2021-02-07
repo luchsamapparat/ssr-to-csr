77186a7fb63b42a87f5d49197f538ec3e522345e
Marvin Luchs <marvin@luchs.org>
2021-02-07 15:23:44 +0100

    Merge remote-tracking branch 'origin/ssr-with-json-api' into spa

--- src/main/java/org/luchs/marvin/ssrtodo/TodoApiController.java
+++ src/main/java/org/luchs/marvin/ssrtodo/TodoApiController.java
@@ -5,16 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
 
 @Controller
 public class TodoApiController {
@@ -33,6 +29,11 @@ public class TodoApiController {
         return ResponseEntity
             .created(URI.create("/tasks"))
             .build();
+    }
+
+    @GetMapping(value = "/tasks/completed", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getCompletedTasksJson() {
+        return ResponseEntity.ok(todoListService.getCompletedTasks());
     }
 
     @PostMapping(value = "/tasks/completed", consumes = MediaType.APPLICATION_JSON_VALUE)
