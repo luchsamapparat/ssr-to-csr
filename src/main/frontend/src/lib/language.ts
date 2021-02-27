3d8142755fc4e7181b5b20205cfcd650114b5196
Marvin Luchs <marvin.luchs@holisticon.de>
2021-02-27 15:52:05 +0100

    fix: read Accept Language header value correctly

--- src/main/frontend/src/lib/language.ts
+++ src/main/frontend/src/lib/language.ts
@@ -6,6 +6,6 @@ export const LanguageContext = React.createContext('en');
 
 export function getLanguage(request: IncomingMessage): string {
     const supportedLocales = new Locales(['en', 'de']);
-    const acceptedLocales = new Locales(request.headers['Accept-Language']);
+    const acceptedLocales = new Locales(request.headers['accept-language']);
     return acceptedLocales.best(supportedLocales).language;
 }
