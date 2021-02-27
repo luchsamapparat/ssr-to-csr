31bc8210f4bb212ac5c06394641943466cb08c1e
Marvin Luchs <marvin.luchs@holisticon.de>
2021-02-27 14:30:37 +0100

    refactor: provide language via context

--- src/main/frontend/src/pages/_app.tsx
+++ src/main/frontend/src/pages/_app.tsx
@@ -1,22 +1,25 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from '../components/navbar';
+import { LanguageContext } from '../lib/language-context';
 
 const CompletedTasks = lazy(() => import('./tasks/completed'));
 const Tasks = lazy(() => import('./index'));
 
 const App = () => (
-    <Router>
-        <Navbar />
-        <main className="container py-5">
-            <Suspense fallback={<div>Loading...</div>}>
-                <Switch>
-                    <Route exact path="/" component={Tasks} />
-                    <Route exact path="/tasks/completed" component={CompletedTasks} />
-                </Switch>
-            </Suspense>
-        </main>
-    </Router>
+    <LanguageContext.Provider value={navigator.language}>
+        <Router>
+            <Navbar />
+            <main className="container py-5">
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch>
+                        <Route exact path="/" component={Tasks} />
+                        <Route exact path="/tasks/completed" component={CompletedTasks} />
+                    </Switch>
+                </Suspense>
+            </main>
+        </Router>
+    </LanguageContext.Provider>
 );
 
 export default App;
