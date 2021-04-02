dd6a14c94fb918662085c70c9b1bffce9f95b837
Marvin Luchs <marvin.luchs@holisticon.de>
2021-04-02 20:35:06 +0200

    fix: typo

--- src/main/frontend/src/components/add-task-form/description-input.tsx
+++ src/main/frontend/src/components/add-task-form/description-input.tsx
@@ -19,7 +19,7 @@ const DescriptionInput: FunctionComponent<DescriptionInputProps> = ({ value, onC
                 id="addTask-description"
                 value={value}
                 className={`form-control description ${getInvalidFormControlCssClass(violations)}`}
-                placeholder="Add as Task..."
+                placeholder="Add a Task..."
                 required
                 minLength={1}
                 onInput={handleInput}
