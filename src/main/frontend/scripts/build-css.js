caf75624993a5e59e7b841364153fd4ad04cceaa
Marvin Luchs <marvin.luchs@holisticon.de>
2021-04-09 15:13:44 +0200

    fix: optimize bootstrap css for react app

--- src/main/frontend/scripts/build-css.js
+++ src/main/frontend/scripts/build-css.js
@@ -3,11 +3,15 @@ import { writeFile } from 'fs/promises';
 import { PurgeCSS } from 'purgecss';
 
 const result = await new PurgeCSS().purge({
-    content: ['../templates/*.html'],
+    content: [
+        './public/**/*.html',
+        './src/**/*.tsx',
+        './src/**/*.ts'
+    ],
     css: ['./node_modules/bootstrap/dist/css/bootstrap.css']
 });
 
 const cleanCSS = new CleanCSS({ returnPromise: true });
 const { styles } = await cleanCSS.minify(result[0].css);
 
-writeFile('../static/styles.css', styles);
+writeFile('./public/styles.css', styles);
